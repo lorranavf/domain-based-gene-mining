f425b7c073143a9be09f712d0c8a07f0978f6060
Lorrana Flores <83467821+lorranavf@users.noreply.github.com>
2023-03-29 10:37:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-# Análise filogenética
+
 import os
 from filo import pfam, metadados_filogeny
 
-# dicionário de dados
+# definindo as variáveis
 
 dicio = {'specie': ['code', 'abreviation']}
- 
 pfam_path = ''
 regex = ''
 seq_path = ''
@@ -14,13 +13,9 @@ seq_list = os.listdir(seq_path)
 
 
 print('\n Obtendo base de dados')
-
 pfam(seq_list=seq_list, seq_path=seq_path, dicio=dicio, pfam_path=pfam_path, regex=regex)
 
-
 print('\n Iniciando análises de prospecção de genes')
-
 outdir = ''
 domain = []
-
 metadados_filogeny(outdir=outdir, domain=domain, seq_list=seq_list, seq_path=seq_path, dicio=dicio, regex=regex)
