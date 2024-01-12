8156e3eec0af3b7c4385d66bd4e38621119c9d74
lorranavf <lorrana.vf@gmail.com>
2024-01-12 17:50:33 -0300

    🚧 updates

--- docs/exemples/run_domain_based.py
+++ docs/exemples/run_domain_based.py
@@ -12,13 +12,14 @@ metadata = Parameters(
             param_seq_path='infiles.domain_based',
             param_seq_dicio=codes,
             param_seq_ext='.faa',
-            param_pfam_in='/home/lcbm/databases/in.pfam/Pfam-A.hmm',
+            # param_pfam_in='/home/lcbm/databases/in.pfam/Pfam-A.hmm',
+            param_pfam_in='/media/lorrana/LorranaHD/Projetos/DB/in.pfam/Pfam-A.hmm',
             param_domain=['Peptidase_S8'],
             param_domain_group=False,
             param_outdir='peptidase.v1',
             param_hmm_analysis=False,
             param_full_analysis=True,
-            param_filogeny_analysis=False,
+            param_filogeny_analysis=True,
             param_orthogroup_analysis=True)
 
 DomainAnalysis(metadata).run()
